f3b343d3d41e870cd9946793e5ebed7c495c0062
Beja <benkrat78@gmail.com>
2025-05-11 21:07:34 +0200

    v1.0.2 Comentarios

--- JoJo's/Model/ArtisticMoments/carrusel.js
+++ JoJo's/Model/ArtisticMoments/carrusel.js
@@ -1,16 +1,23 @@
+//Espera a que el html esté cargado para empezar
 document.addEventListener('DOMContentLoaded', () => {
+    //Selecciona carrusel y elemento
     const carrusel = document.querySelector('.carrusel');
     const elementos = document.querySelectorAll('.elemento');
+    //Nos lleva al primer elemento del carrusel
     let currentIndex = 0;
 
     //Centra elemento
     const centrarElemento = () => {
+        //Selecciona el contenedor principal de carrusel
         const container = document.querySelector('.carrusel-container');
+        //Encuentra elemento en el que estamos usando currentIndex
         const elemento = elementos[currentIndex];
+        //Obtiene el ancho visible del contenedor
         const containerWidth = container.offsetWidth;
+        //Posición horizontal del elemento respecto al borde izquierdo
         const elementoLeft = elemento.offsetLeft;
+        //Ancho del elemento
         const elementoWidth = elemento.offsetWidth;
-
         //Calcula la posición para centrar
         const scrollTo = elementoLeft - (containerWidth / 2) + (elementoWidth / 2);
         carrusel.style.transform = `translateX(-${scrollTo}px)`;
